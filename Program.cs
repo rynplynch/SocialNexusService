186914a8fb38d4274e546a72ba43874283af4b56
Ryan Lynch <rynplynch@gmail.com>
2024-08-04 17:44:34 -0700

    configure how claims are mapped

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Abstractions;
@@ -12,19 +14,23 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // This is required to be instantiated before the OpenIdConnectOptions starts getting configured.
+        // By default, the claims mapping will map claim names in the old format to accommodate older SAML applications.
+        // For instance, 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' instead of 'roles' claim.
+        // This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
+        JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
         // Add services to the container.
         builder
             .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -32,6 +38,7 @@ public class Program
             app.UseHttpsRedirection();
         }
 
+        app.UseHsts();
         app.UseAuthentication();
 
         app.UseAuthorization();
