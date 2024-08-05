b626b84f4c7567b797c11f32f19ce5c0500e639f
Ryan Lynch <rynplynch@gmail.com>
2024-08-04 17:37:56 -0700

    endpoint now returns entire model

--- Controllers/TimeController.cs
+++ Controllers/TimeController.cs
@@ -20,8 +20,15 @@ public class TimeController : ControllerBase
     {
         // c# class for handling dates and times
         // get the current date time value
-        DateTime t = DateTime.Now;
+        DateTime now = DateTime.Now;
 
-        return Ok(t.TimeOfDay);
+        // also pass a formated version
+        TimeSpan formated = now.TimeOfDay;
+
+        // construct the Time object to return
+        Time t = new Time() { Now = now, Formated = formated };
+
+        // return status 200 along with the Time object
+        return Ok(t);
     }
 }
